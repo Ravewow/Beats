f52aa89e20e24dbae0918ee8c0d0e25efeeaa0fc
Sergey S <paniqwow@gmail.com>
2023-02-01 17:45:25 +0300

    edits accardion menu team section

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+  $(".portfolio__name-wrapper").on("click", (e) => {
+    let elem = $(e.currentTarget).next();
+
+    elem.slideToggle();
+  });
+
+  $(".portfolio__name").on("click", function(e) {
+    let elem = $(e.currentTarget).next();
+    elem.toggleClass("portfolio__name-triangle--rotate")
+  })
+});
